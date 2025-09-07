2df7d1587f234c4d5aea361cd0cb50d09a2cda05
dzulfikar2403 <dzulfikar2403@gmail.com>
2025-09-07 19:57:39 +0700

    feat: check auth

--- api/authApi.ts
+++ api/authApi.ts
@@ -1,12 +1,17 @@
 'use server'
 import { signIn, signOut } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const loginGoogle = async () => {
-    await signIn("google",{redirectTo: '/dashboard/home'});
+    await signIn("google");
+
+    redirect('/dashboard/home')
 }
 
 export const loginGithub = async () => {
-    await signIn("github",{redirectTo: '/dashboard/home'});
+    await signIn("github");
+    
+    redirect('/dashboard/home')
 }
 
 export const logout = async () => {
